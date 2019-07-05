b3fbe640d4e4c34b1bf8825df4b221eadc287f6c
EdwardGonzalez <45890833+EdwardGonzalez@users.noreply.github.com>
2019-07-04 23:31:43 -0500

    edward Update

--- flask project/routes/edroute.py
+++ flask project/routes/edroute.py
@@ -134,7 +134,11 @@ def workorders():
                    "count": {"$sum":1}
                }
             },
-            
+                        {
+                '$sort': {
+                    'count': -1
+                }
+            }, 
         ]
     
     workOrdersDBDocs = db.workorders.aggregate(_pipeline)
@@ -186,6 +190,43 @@ def user():
     workOrdersDBDocs = db.users.aggregate(_pipeline)
     df = pd.DataFrame(list(workOrdersDBDocs)).fillna(0)
     df = df.set_index('id')
+    result = {
+        'data':  json.loads(df.to_json(orient='records'))
+    }
+    return jsonify(result)
+
+
+@bp.route("/plants")
+def plants():
+    db = dbClient["84ad9547-499c-40e3-a02e-fcf4f8714871"]
+
+    _pipeline = [
+            {
+                '$match': {
+                    # **match
+                    # "companyId"
+                }
+            }, 
+            {
+                '$skip': 0
+            }, 
+            {
+                '$project': {
+                    '_id': 0,
+                    'id': 1,
+                    'name':1
+                }
+            }
+            #             {
+            #    "$group":{
+            #        "_id":"$id",
+            #        "name": "$name"
+            #    }
+            #}
+        ]
+    
+    plantsDBDocs = db.plants.aggregate(_pipeline)
+    df = pd.DataFrame(list(plantsDBDocs)).fillna(0)
     result = {
         'data':  json.loads(df.to_json(orient='records'))
     }
--- flask project/static/js/escript.js
+++ flask project/static/js/escript.js
@@ -2,42 +2,49 @@
 escript = async (accesstoken, accesstokenidp) => {
     const workOrders = await d3.json("/edroute/workorders");
     const operations = await d3.json("/edroute/operations");
+    const plants = await d3.json("/edroute/plants");
 
-     var xValue = [];
-     var yValue = [];
+console.log(plants)
+// var result = _.unionBy(workOrders.data, plants.data, "_id");
 
-     for (var i = 0; i < workOrders.data.length; i++) {
-            xValue.push(workOrders.data[i]._id);
-            yValue.push(workOrders.data[i].count);
-    }
+// console.log(result);
+
+
+//      var xValue = [];
+//      var yValue = [];
+
+//      for (var i = 0; i < workOrders.data.length; i++) {
+//             xValue.push(workOrders.data[i]._id);
+//             yValue.push(workOrders.data[i].count);
+//     }
    
-    var trace1 = {
-      x: xValue,
-      y: yValue,
-      type: 'bar',
-      text: yValue.map(String),
-      textposition: 'auto',
-      hoverinfo: 'none',
-      marker: {
-        color: 'rgb(158,202,225)',
-        opacity: 0.6,
-        line: {
-          color: 'rgb(8,48,107)',
-          width: 1.5
-        }
-      }
-    };
+//     var trace1 = {
+//       x: xValue,
+//       y: yValue,
+//       type: 'bar',
+//       text: yValue.map(String),
+//       textposition: 'auto',
+//       hoverinfo: 'none',
+//       marker: {
+//         color: 'rgb(158,202,225)',
+//         opacity: 0.6,
+//         line: {
+//           color: 'rgb(8,48,107)',
+//           width: 1.5
+//         }
+//       }
+//     };
     
-    var data = [trace1];
+//     var data = [trace1];
     
-    var layout = {
-      title: 'January 2013 Sales Report'
-    };
+//     var layout = {
+//       title: 'January 2013 Sales Report'
+//     };
     
-    Plotly.newPlot('eddiv', data, layout);
+//     Plotly.newPlot('eddiv', data, layout);
         
     
+// }
+
+
 }
-
-
-
