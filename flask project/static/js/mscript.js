22fb40a673f05369ade7770cd9a75de2b2bcb9a6
MarthaMeses <46548909+MarthaMeses@users.noreply.github.com>
2019-07-05 02:20:18 -0500

    Update mscript.js

--- flask project/static/js/mscript.js
+++ flask project/static/js/mscript.js
@@ -1,6 +1,31 @@
 
 mscript = async (accesstoken, accesstokenidp) => {
-    const workOrders = await d3.json("/maroute/workorders");
-    const operations = await d3.json("/maroute/operations");
-    
+    // const workOrders = await d3.json("/maroute/workorders");
+    // const operations = await d3.json("/maroute/operations");
+
+    const data = await d3.json("/maroute/workorders");
+    var xValues = [];
+    var yValues = [];
+    for(var i=0; i<data.data.length; i++){
+        xValues.push(data.data[i].count);
+        yValues.push(data.data[i]._id);
+    }
+
+    statusWorkOrdersChart(xValues,yValues);
 }
+
+function statusWorkOrdersChart(xValues,yValues) {
+    var trace1={
+    // labels: ['NEW','APPROVED','CLOSED','UPDATED','SUSPENDED','PRIORITY'],
+        labels: yValues,
+        values: xValues, 
+        type:'pie'
+    };
+    var dataT=[trace1];
+    var layout = {
+        // title:'PIE',
+        height: 400,
+        width: 400
+      };
+    Plotly.newPlot('madiv',dataT,layout);
+};
