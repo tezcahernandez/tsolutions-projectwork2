0e355539cd222d567b55896ce22822904713dd7f
EdwardGonzalez <45890833+EdwardGonzalez@users.noreply.github.com>
2019-07-04 21:09:34 -0500

    Update escript.js

--- flask project/static/js/escript.js
+++ flask project/static/js/escript.js
@@ -2,35 +2,33 @@
 escript = async (accesstoken, accesstokenidp) => {
     const workOrders = await d3.json("/edroute/workorders");
     const operations = await d3.json("/edroute/operations");
-console.log(workOrders.count)
 
+     var dataPoints = [];
 
-    //  var dataPoints = [];
+     var chart = new CanvasJS.Chart("eddiv", {
+         animationEnabled: true,
+         theme: "light2",
+         title: {
+             text: "Daily Sales Data"
+         },
+         axisY: {
+             title: "Units",
+             titleFontSize: 24
+         },
+         data: [{
+             type: "column",
+             yValueFormatString: "#,### Units",
+             dataPoints: dataPoints
+         }]
+     });
 
-    //  var chart = new CanvasJS.Chart("chartContainer", {
-    //      animationEnabled: true,
-    //      theme: "light2",
-    //      title: {
-    //          text: "Daily Sales Data"
-    //      },
-    //      axisY: {
-    //          title: "Units",
-    //          titleFontSize: 24
-    //      },
-    //      data: [{
-    //          type: "column",
-    //          yValueFormatString: "#,### Units",
-    //          dataPoints: dataPoints
-    //      }]
-    //  });
-
-    //  for (var i = 0; i < workOrders.length; i++) {
-    //      dataPoints.push({
-    //          x: workOrders[i].plantId,
-    //          y: workOrders[i].count
-    //      });
-    //  }
-    //  chart.render();
+     for (var i = 0; i < workOrders.workOrders.length; i++) {
+         dataPoints.push({
+             x: workOrders.workOrders[i].plantId,
+             y: workOrders.workOrders[i].count
+         });
+     }
+     chart.render();
 
 }
 
