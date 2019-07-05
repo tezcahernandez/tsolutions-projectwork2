52bb225fe3f66382e3e2c5d571ed748f51af3c42
EdwardGonzalez <45890833+EdwardGonzalez@users.noreply.github.com>
2019-07-04 20:24:39 -0500

    Update escript.js

--- flask project/static/js/escript.js
+++ flask project/static/js/escript.js
@@ -2,33 +2,35 @@
 escript = async (accesstoken, accesstokenidp) => {
     const workOrders = await d3.json("/edroute/workorders");
     const operations = await d3.json("/edroute/operations");
+console.log(workOrders.count)
 
-     var dataPoints = [];
 
-     var chart = new CanvasJS.Chart("chartContainer", {
-         animationEnabled: true,
-         theme: "light2",
-         title: {
-             text: "Daily Sales Data"
-         },
-         axisY: {
-             title: "Units",
-             titleFontSize: 24
-         },
-         data: [{
-             type: "column",
-             yValueFormatString: "#,### Units",
-             dataPoints: dataPoints
-         }]
-     });
+    //  var dataPoints = [];
 
-     for (var i = 0; i < workOrders.length; i++) {
-         dataPoints.push({
-             x: workOrders[i].plantId,
-             y: workOrders[i].count
-         });
-     }
-     chart.render();
+    //  var chart = new CanvasJS.Chart("chartContainer", {
+    //      animationEnabled: true,
+    //      theme: "light2",
+    //      title: {
+    //          text: "Daily Sales Data"
+    //      },
+    //      axisY: {
+    //          title: "Units",
+    //          titleFontSize: 24
+    //      },
+    //      data: [{
+    //          type: "column",
+    //          yValueFormatString: "#,### Units",
+    //          dataPoints: dataPoints
+    //      }]
+    //  });
+
+    //  for (var i = 0; i < workOrders.length; i++) {
+    //      dataPoints.push({
+    //          x: workOrders[i].plantId,
+    //          y: workOrders[i].count
+    //      });
+    //  }
+    //  chart.render();
 
 }
 
