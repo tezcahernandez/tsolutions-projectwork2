d778bc5e5f970d20e03d1db1bd2268601ad68239
MarthaMeses <46548909+MarthaMeses@users.noreply.github.com>
2019-07-04 23:31:58 -0500

    Update maroute.py

--- flask project/routes/maroute.py
+++ flask project/routes/maroute.py
@@ -188,3 +188,47 @@ def user():
         'data':  json.loads(df.to_json(orient='records'))
     }
     return jsonify(result)
+
+@bp.route("/businessunits")
+def businessunits():
+    db = dbClient["84ad9547-499c-40e3-a02e-fcf4f8714871"]
+
+    _pipeline = [
+            {
+                '$match': {
+                    # **match
+                    # "companyId"
+                }
+            }, 
+            {
+                '$sort': {
+                    'createdDate': -1
+                }
+            }, 
+            {
+                '$skip': 0
+            }, 
+            # {
+            #     '$limit': 50
+            # },
+            {
+                '$project': {
+                    '_id':0,
+                    'id':1,
+                    'companyId':1,
+                    'name':1,
+                    'description':1,
+                    'as':1,
+                    'plantId':1
+                }
+            }
+            
+        ]
+    
+    workOrdersDBDocs = db.businessunits.aggregate(_pipeline)
+    df = pd.DataFrame(list(workOrdersDBDocs)).fillna(0)
+    # df = df.set_index('id')
+    result = {
+        'data':  json.loads(df.to_json(orient='records'))
+    }
+    return jsonify(result)
