4e6c49bf47699636f7c3c591cae97c85c0b38e34
EdwardGonzalez <45890833+EdwardGonzalez@users.noreply.github.com>
2019-07-05 14:30:17 -0500

    edwrd Update

--- flask project/static/js/escript.js
+++ flask project/static/js/escript.js
@@ -3,28 +3,35 @@ escript = async (accesstoken, accesstokenidp) => {
     const workOrders = await d3.json("/edroute/workorders");
     const operations = await d3.json("/edroute/operations");
     const plants = await d3.json("/edroute/plants");
-
-var grafica1 = _.map(workOrders.data, function(item) {
-    return _.assign(item, _.find(plants.data, ['_id', item._id]));
-});
+    console.log(workOrders)
+    console.log(plants)
 
 
-    var xValue = [];
-     var yValue = [];
+    const grafica1ed = workOrders.data.map(it1 => {
+        it1.name = plants.data.find(it2 => it2._id === it1._id).name
+        return it1
+      })
 
-     for (var i = 0; i < grafica1.length; i++) {
-            xValue.push(grafica1[i].name);
-            yValue.push(grafica1[i].count);
+//    var grafica1ed = _.map(workOrders.data, function(itemed) {
+//       return _.assign(itemed, _.find(plants.data, ['_id', itemed._id]));
+//   });
+
+    var xValueGrafica1Ed = [];
+     var yValueGrafica1Ed = [];
+
+     for (var i = 0; i < grafica1ed.length; i++) {
+            xValueGrafica1Ed.push(grafica1ed[i].name);
+            yValueGrafica1Ed.push(grafica1ed[i].count);
     }
-   
-console.log(xValue)
-console.log(yValue)
+console.log(grafica1ed)
+console.log(xValueGrafica1Ed)
+console.log(yValueGrafica1Ed)
 
     var trace1 = {
-      x: xValue,
-      y: yValue,
+      x: xValueGrafica1Ed,
+      y: yValueGrafica1Ed,
       type: 'bar',
-      text: yValue.map(String),
+      text: yValueGrafica1Ed.map(String),
       textposition: 'auto',
       hoverinfo: 'none',
       marker: {
